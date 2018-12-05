552c6a85198d9b27607b4ce7e60b0c810e7b0683
Rabins Sharma Lamichhane <support@rabinsxp.com>
2018-12-05 15:58:17 +0530

    menu navigation fixed

--- system/header_top_menu.php
+++ system/header_top_menu.php
@@ -2,23 +2,23 @@
   <div class="col-sm-9 h_menu4">
     <ul class="megamenu skyblue">
       <li class="active grid">
-        <a class="color8" href="/index.php">Home
+        <a class="color8" href="index.php">Home
         </a>
       </li>
       <li>
-        <a class="color1" href="/products.php#cameras">Camera
+        <a class="color1" href="products.php#cameras">Camera
         </a>
       </li>
       <li>
-        <a class="color2" href="/products.php#clothes">Clothes
+        <a class="color2" href="products.php#clothes">Clothes
         </a>
       </li>
       <li>
-        <a class="color3" href="/products.php#watches">Watches
+        <a class="color3" href="products.php#watches">Watches
         </a>
       </li>
       <li>
-        <a class="color4" href="/products.php">All Products
+        <a class="color4" href="products.php">All Products
         </a>
       </li>
     </ul>
